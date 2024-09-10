5e5b20583065a8d2d273676d71975a03b1eb7959
O-01 <sa.an-s@yandex.com>
2024-09-10 13:06:50 -0500

    Saving progress on sha256 - local testing successful (crypto)

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -10,8 +10,13 @@
 uint8_t *sha256(
 	int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-	;
+	SHA256_CTX context;
 
-	if (!digest)
+	if (!s || !len || !digest)
 		return (NULL);
+	if (!SHA256_Init(&context) ||
+		!SHA256_Update(&context, s, len) ||
+		!SHA256_Final(digest, &context))
+		return (NULL);
+	return (digest);
 }
