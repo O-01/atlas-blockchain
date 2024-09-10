399f582031f710bc836acfd39e32da2fc6134684
O-01 <sa.an-s@yandex.com>
2024-09-10 14:13:31 -0500

    Saving progress on ec_to_pub - invalid reads present (crypto)

---
+++ crypto/ec_to_pub.c
@@ -0,0 +1,16 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_to_pub - extracts public key from EC_KEY opaque structure
+ * @key: pointer to EC_KEY structure from which to retrieve public key - if
+ *       NULL, function must do nothing and fail
+ * @pub: address at which to store extracted public key (key shall not be
+ *       compressed)
+ * Return: pointer to `pub` upon success, otherwise NULL
+ */
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
+{
+	if (!key)
+		return (NULL);
+	return (memcpy(pub, EC_KEY_get0_public_key(key), EC_PUB_LEN), pub);
+}
