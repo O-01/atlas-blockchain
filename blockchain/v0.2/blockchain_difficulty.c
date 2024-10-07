623f0bf8770c1ec8056e481fde941823e7e85d7d
O-01 <sa.an-s@yandex.com>
2024-10-07 18:22:46 -0500

    Simplified blockchain_difficulty (blockchain/v0.2)

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -1,7 +1,5 @@
 #include "blockchain.h"
 
-static uint32_t last_adj_idx(blockchain_t const *blockchain);
-
 /**
  * blockchain_difficulty - computes difficulty to assign to potential next
  *                         Block in Blockchain
@@ -19,25 +17,10 @@ uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 		block->info.index < DIFFICULTY_ADJUSTMENT_INTERVAL ||
 		NO_INTERVAL_ADJUSTMENT(block->info.index))
 		return (block->info.difficulty);
-	last_adj = llist_get_node_at(blockchain->chain, last_adj_idx(blockchain));
-	if (ACTUAL(block, last_adj) < (EXPECTED >> 1))
+	last_adj = llist_get_node_at(blockchain->chain, LAST_ADJ(blockchain->chain));
+	if (ACTUAL(block, last_adj) < (EXPECTED(block, last_adj) >> 1))
 		return (block->info.difficulty + 1);
-	else if (ACTUAL(block, last_adj) > (EXPECTED << 1))
+	else if (ACTUAL(block, last_adj) > (EXPECTED(block, last_adj) << 1))
 		return (block->info. difficulty - 1);
 	return (block->info.difficulty);
-}
-
-/**
- * last_adj_idx - retrieves index of last block with adjusted difficulty
- * @blockchain: points to Blockchain to search in
- * Return: index of last block with adjusted difficulty
- */
-static uint32_t last_adj_idx(blockchain_t const *blockchain)
-{
-	uint32_t size = 0;
-
-	if (!blockchain)
-		return (-1);
-	size = llist_size(blockchain->chain);
-	return (LAST_ADJUSTED(size));
 }
