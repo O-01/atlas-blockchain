80aa74d75ac77aa447c7053c7bd22a69261a8ec5
O-01 <sa.an-s@yandex.com>
2024-10-07 15:46:24 -0500

    Saving progress on block_mine - local testing successful (blockchain/v0.2)

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -7,5 +7,13 @@
  */
 void block_mine(block_t *block)
 {
-	;
+	uint8_t *hash = NULL;
+
+	if (!block)
+		return;
+	for (hash = block_hash(block, block->hash);
+		!hash_matches_difficulty(hash, block->info.difficulty);
+		++block->info.nonce, hash = block_hash(block, block->hash))
+		;
+	memcpy(block->hash, hash, SHA256_DIGEST_LENGTH);
 }
