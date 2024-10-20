3ced77c85618904560e8bc0388c84460d4f658d9
O-01 <sa.an-s@yandex.com>
2024-10-20 02:23:27 -0500

    Saving progress on unspent_tx_out_create - local testing successful (blockchain/v0.3)

---
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -0,0 +1,30 @@
+#include "transaction.h"
+
+/**
+ * unspent_tx_out_create - allocates and initializes unspent transaction output
+ *                         structure
+ * @block_hash: contains hash of Block where referenced transaction output is
+ *              located
+ * @tx_id: contains hash of transaction in Block block_hash, where referenced
+ *         transaction output is located
+ * @out: pointer to referenced transaction output
+ * Return: pointer to created unspent transaction output upon success,
+ *         otherwise NULL
+ */
+unspent_tx_out_t *unspent_tx_out_create(
+	uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	uint8_t tx_id[SHA256_DIGEST_LENGTH],
+	tx_out_t const *out)
+{
+	unspent_tx_out_t *add = NULL;
+
+	if (!block_hash || !tx_id || !out)
+		return (NULL);
+	add = calloc(1, sizeof(unspent_tx_out_t));
+	if (!add)
+		return (NULL);
+	memcpy(add->block_hash, block_hash, SHA256_DIGEST_LENGTH);
+	memcpy(add->tx_id, out->hash, SHA256_DIGEST_LENGTH);
+	add->out = *out;
+	return (add);
+}
