f827e0a0bde9c17f8c2af329d1e29d3165bb5f32
O-01 <sa.an-s@yandex.com>
2024-10-22 17:01:31 -0500

    Modified block_destroy with transaction_destroy destructor function pointer input to llist_destroy (blockchain/v0.3)

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -1,7 +1,5 @@
 #include "blockchain.h"
 
-void tx_destroy(void *tx);
-
 /**
  * block_destroy - deletes existing Block
  * @block: points to Block to delete
@@ -11,21 +9,6 @@ void block_destroy(block_t *block)
 	if (!block)
 		return;
 	if (block->transactions)
-		llist_destroy(block->transactions, 0, tx_destroy);
+		llist_destroy(block->transactions, 1, (node_dtor_t)&transaction_destroy);
 	FREE_0(block);
-}
-
-/**
- * tx_destroy - deallocates tx structure
- * @tx: points to tx to delete
- */
-void tx_destroy(void *tx)
-{
-	if (!tx)
-		return;
-	if (TX(tx)->inputs)
-		llist_destroy(TX(tx)->inputs, (llist_size(TX(tx)->inputs) > 0), NULL);
-	if (TX(tx)->outputs)
-		llist_destroy(TX(tx)->outputs, (llist_size(TX(tx)->outputs) > 0), NULL);
-	FREE_0(tx);
 }
